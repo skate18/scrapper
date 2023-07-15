0a11826533afdc706f42a81e56f1df9d0c8da644
Github User <msi.mob.contacts@gmail.com>
2023-07-15 21:10:31 +0600

    fake company data

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Company;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i <= 5; $i++) {
+            $company = new Company();
+            $company->setName('Test Company ' . $i);
+            $company->setRegistrationCode(mt_rand(103341878, 903341878));
+            $company->setAddress("Test address " . $i);
+            $manager->persist($company);
+        }
+        $manager->flush();
+    }
+}
