eed6c309b73da6819489c563fd18a0264b058bbb
Github User <msi.mob.contacts@gmail.com>
2023-07-16 21:42:06 +0600

    create test data

--- assets/datatable.js
+++ assets/datatable.js
@@ -9,16 +9,17 @@ let actions = {
                 { title: "Name", data : "name" },
                 { title: "Registration Code", data : "registrationCode" },
                 { title: "Address", data : "address" },
-                { 
+                {
                     title: 'Actions',
-                    render: (data,type,row) => {
-                      return `<span role="button" class='p-2 mr-2 edit' data-id='${row.id}' title='Edit'><i class="fas fa-edit"></i> <span>
-                      <span role="button" class='p-2 delete' data-id='${row.id}' title='Delete'><i class="fas fa-trash"></i> <span>`;
-                    }
-                 }
-            ]
+                    render: (data, type, row) => {
+                        return `<span role="button" class='p-2 mr-1 edit' data-id='${row.id}' title='Edit'><i class="fas fa-edit"></i></span>
+                                <span role="button" class='p-2 delete' data-id='${row.id}' title='Delete'><i class="fas fa-trash"></i></span>`;
+                    },
+                    orderable: false
+                }
+            ],
+            destroy: true,
         });
-         console.log(table);
     },
     scrape: function () {
         $('body').on('click', '#scrape', function(){
@@ -27,7 +28,24 @@ let actions = {
     },
     addTestData: function () {
         $('body').on('click', '#add-test-data', function(){
-            console.log('CLicked to add test data');
+            $.ajax({
+                url: '/companies/create',
+                type: 'POST',
+                success: function(response) {
+                    if(response.success){
+                        actions.showCompanyData();
+                    }else{
+                        actions.showErrorMessage();
+                    }
+                }
+            });
+        });
+    },
+    showErrorMessage: function () {
+        iziToast.error({
+            title: 'Error',
+            message: 'An error has occurred. Please try again later.',
+            position: 'topCenter'
         });
     },
 }; // END actions
--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -7,13 +7,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 
 class CompanyController extends AbstractController
 {
 
-    #[Route('/companies', name: 'app_companies')]
+    #[Route('/companies', name: 'app_companies', methods: ['GET'])]
     public function getCompanies(EntityManagerInterface $em, SerializerInterface $serializer): JsonResponse
     {
         $repository = $em->getRepository(Company::class);
@@ -21,5 +22,27 @@ class CompanyController extends AbstractController
         $json = $serializer->serialize($companies, JsonEncoder::FORMAT, ['json_encode_options' => JSON_UNESCAPED_UNICODE]);
 
         return new JsonResponse($json, 200, [], true);
+    }
+
+    #[Route('/companies/create', name: 'app_companies_create', methods: ['POST'])]
+    public function create(EntityManagerInterface $entityManager): JsonResponse
+    {
+        $json = ['success' => false];
+
+        try{
+            $testNum = mt_rand(6, 9);
+            $company = new Company();
+            $company->setName('Test Company ' . $testNum);
+            $company->setRegistrationCode(mt_rand(103341878, 903341878));
+            $company->setAddress("Test address " . $testNum);
+            $company->setCreatedAt(new \DateTimeImmutable());
+            $entityManager->persist($company);
+            $entityManager->flush();
+            $json = ['success' => true];
+        }catch(Exception $e){
+
+        }
+
+        return new JsonResponse($json);
     }
 }
